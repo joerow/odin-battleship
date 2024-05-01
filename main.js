9ec2084a2e24f7ec3aac8d2786ccd13b29877f41
joerow <joerow@gmail.com>
2024-05-01 18:32:49 +0800

    started adding user interface

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ import { Player } from "./battleship";
 import { buildElement } from "./domhandler";
 
 let app = document.querySelector("#app");
+
+let title = buildElement("div", "title", "", "BATTLESHIP");
+app.appendChild(title);
+
+let message = buildElement(
+  "div",
+  "message",
+  "",
+  "This is the message area. There will be changing instructions here"
+);
+app.appendChild(message);
+
+message.textContent = "new message";
