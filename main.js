e2c81c67a856f62bee87cfa85da5c8f487434f4b
joerow <joerow@gmail.com>
2024-06-03 21:27:32 +0800

    start to add grid

--- main.js
+++ main.js
@@ -18,9 +18,25 @@ message.textContent = "Let's play";
 
 let newGame = buildElement("button", "new-game", "", "new game");
 app.appendChild(newGame);
+
 newGame.onclick = function () {
   let p1 = new Player("User", 1);
   let p2 = new Player("CPU", 2);
   newGame.style.display = "none";
   message.textContent = "Let's go";
+  let gameboards = buildElement("div", "gameboards", "", "");
+  app.appendChild(gameboards);
+  let p1Gameboard = buildElement("div", "p1-gameboard", "", "");
+  for (let i = 0; i < p1.gameboard.size; i++) {
+    for (let j = 0; j < p1.gameboard.size; j++) {
+      let gameboardTile = buildElement(
+        "button",
+        "x" + i + "y" + j,
+        "gameboard-button",
+        "X"
+      );
+      p1Gameboard.appendChild(gameboardTile);
+    }
+  }
+  gameboards.appendChild(p1Gameboard);
 };
